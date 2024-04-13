74894e6785eba96fbe53a3c0a03f695cb15c4fa2
Chris-Kau <uhchris04@gmail.com>
2024-04-13 15:55:27 -0700

    error message if user doesnt set token

--- responses.py
+++ responses.py
@@ -6,6 +6,15 @@ import todo
 def get_response(user_input):
     lowered: str = user_input.lower()
     print("CHECKED", main.checked)
+    if "settoken" in user_input.lower()[:9]:
+        user = userobject(user_input[9:])
+        if user.check_token() == False:
+            return "Invalid Access Token"
+        main.todo_list = []
+        for i in range(len(course_list)):
+            main.todo_list.append({"index": str(i), "item": str(course_list[i]), "isChecked": "False"})
+        main.checked = 1
+        return "success!"
     if main.checked == 1:
         if "hello" in lowered:
             return "hello there!"
@@ -15,6 +24,7 @@ def get_response(user_input):
         if "check" in user_input.lower()[:6]:
             return todo.checktodo(user_input[6:])
     else:
+<<<<<<< Updated upstream
         if "settoken" in user_input.lower()[:9]:
             user = userobject(user_input[9:])
             if user.check_token() == False:
@@ -24,3 +34,6 @@ def get_response(user_input):
                 main.todo_list.append({"index": str(i), "item": str(assignment_list[i]), "isChecked": "False"})
             main.checked = 1
             return "success!"
+=======
+        return ("Please set your canvas api token using !settoken (your token)")
+>>>>>>> Stashed changes
