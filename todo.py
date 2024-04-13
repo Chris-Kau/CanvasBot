c1dea5386cd2b310e7b1cdca01aa504d25eb64a3
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2024-04-13 14:39:19 -0700

    Update todo.py

--- todo.py
+++ todo.py
@@ -5,8 +5,8 @@ todo_list = [] # list of dicts
 
 def convertstring(listdict):
     text = ""
-    for i, dict in enumerate(listdict):
-        for key, value in dict.items():
+    for i, dict in enumerate(listdict): # for dict in list of dicts
+        for key, value in dict.items(): # for pairs in dict. find a way to strikethorough? 
             text = text + value + " "
         text += "\n"
     return text
@@ -16,23 +16,28 @@ def todolist():
     print("---------------------------------")
 
     for i in range(len(course_list)):
-        todo_list.append({"index": str(i), "item": str(course_list[i])}) # appends every time u look at todo list :( 
-
+        dicttemp = {"index": str(i), "item": str(course_list[i]), "isChecked": False}
+        if todo_list[i] != dicttemp:
+            todo_list.append(dicttemp) # appends every time u look at todo list :( fixed?
+        
     # print(convertstring(todo_list))
     return convertstring(todo_list)
 
+# have isChecked be a thing
+# have todolist func check if item is already there or not, if not then add it
+# print todolist with isChecked being strikethroughed
 
-def checktodo(num):
-    # for i in range(len(todo_list)):
-    #     if todo_list[i]["index"] == num:
-    #         del todo_list[i]
-    #         break
-
-    for i, d in enumerate(todo_list):
-        if d['index'] == num:
-            id = i
+def checktodo(num): # isnt removing the item properly :/
+    for i in range(len(todo_list)):
+        if todo_list[i]["index"] == num:
+            todo_list[i].isChecked = True
             break
-    if id is not None:
-        todo_list.pop(id)
+
+    # for i, d in enumerate(todo_list):
+    #     if d['index'] == num:
+    #         id = i
+    #         break
+    # if id is not None:
+    #     todo_list.pop(id)
     return convertstring(todo_list)
 
