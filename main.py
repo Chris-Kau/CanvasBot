fc14ffa78fe7ee77de63ced7b447122821c68e3a
Chris-Kau <uhchris04@gmail.com>
2024-04-13 11:49:56 -0700

    bot outline

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
+#basic message response
+async def send_message(message, user_message):
+    if not user_message:
+        return
+    if is_private := user_message[0] == '!':
+        user_message = user_message[1:]
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+#startup message
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running")
+
+#handle incoming messages (so bot doesnt read its own message)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+    print(f"{channel} {username} {user_message}")
+    await send_message(message, user_message)
+
+def main():
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,4 @@
-def get_response(user_input: str) -> str:
-    raise NotImplementedError
+def get_response(user_input):
+    lowered: str = user_input.lower()
+    if "hello" in lowered:
+        return "hello there!"
