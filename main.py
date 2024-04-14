658d389dd702790b78a310879484aa7563cf3530
Chris-Kau <uhchris04@gmail.com>
2024-04-14 05:03:47 -0700

    added some useful basic commands
    cats...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # pip install python-dotenv
 # pip install discord
+import json
 import os
 from dotenv import load_dotenv
 import discord
@@ -10,10 +11,11 @@ import datetime
 from timeconverter import time_to_word
 from classassignments import get_assignment_list
 import copy
-
+import requests
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+TENOR_KEY = os.getenv('TENOR_KEY')
 intents = Intents.default()
 intents.message_content = True
 client = Client(intents=intents)
@@ -34,7 +36,9 @@ async def on_ready() -> None:
 async def send_message(message, user_message):
     if not user_message:
         return
-    if user_message[0] == '!':
+    if user_message == '!reminder' and open("token_state.txt").read() == "1":
+        reminder.start(message)
+    elif user_message[0] == '!':
         user_message = user_message[1:]
         try:
             response = responses.get_response(user_message)
@@ -42,12 +46,28 @@ async def send_message(message, user_message):
                 embed = discord.Embed(title="To-Do List", description=response[0], color=discord.Color.purple())
                 embed.set_footer(text=response[1])
                 await message.channel.send(embed=embed)
+            elif "help" == user_message.lower():
+                helpembed = discord.Embed(title = "List of Commands", description=response, color = discord.Color.purple())
+                helpembed.set_footer(text="NOTE: !settoken is **REQUIRED** to access other commands apart from !help and !guide... and !cats")
+                await message.channel.send(embed=helpembed)
+            elif "guide" == user_message.lower():
+                guideembed = discord.Embed(title = "How To Get Your Access Token", description=response, color = discord.Color.purple())
+                image1 = discord.Embed()
+                image2 = discord.Embed()
+                image3 = discord.Embed()
+                image1.set_image(url='https://cdn.discordapp.com/attachments/1099064514298781748/1229024485269835836/image.png?ex=662e2d3f&is=661bb83f&hm=b50bf6a0a88dcbc3b9a980ffca8ccb374590e869d40c3a698352e8e1d196fa4d&')
+                image2.set_image(url='https://cdn.discordapp.com/attachments/1099064514298781748/1229025286994006026/image.png?ex=662e2dfe&is=661bb8fe&hm=845ba018b8ac97a1e1f870422407cf236dce492faeeb733bd412e1005db5cd49&')
+                image3.set_image(url='https://cdn.discordapp.com/attachments/1099064514298781748/1229025574979375144/image.png?ex=662e2e43&is=661bb943&hm=6363fa3214fef6812a063ecd28a72add8a73c955bc28d8efd1d62a8b2fdb641b&')
+                await message.channel.send(embeds = [guideembed, image1, image2, image3])
+            elif "cats" == user_message.lower():
+                r = requests.get('https://api.thecatapi.com/v1/images/search?format=json&mime_types=gif')
+                cats = json.loads(r.text)
+                await message.channel.send(cats[0]['url'])
             else:
                 await message.channel.send(response)
         except Exception as e:
             print(e)
-    elif user_message == '?reminder' and open("token_state.txt").read() == "1":
-        reminder.start(message)
+
 
 
 #handle incoming messages (so bot doesnt read its own message)
--- responses.py
+++ responses.py
@@ -8,45 +8,51 @@ import copy
 
 def get_response(user_input):
     lowered: str = user_input.lower()
-    #writes the user's assignments to 2 files, one is use for crossing out an assignment, other is used for storing data
-    if "settoken" in user_input.lower()[:9]:
-        with open("user_token.txt", "w") as file:
-            file.write(user_input[9:])
-        user = userobject(open("user_token.txt").read())
-        if user.check_token() == False:
-            with open("token_state.txt", "w") as file:
-                file.write('0')
-            return "Invalid Access Token"
-        f = open("bleh.txt", "w")
-        f2 = open("bleh2.txt", "w")
-        global assignments
-        assignments = copy.copy(list(classassignments.get_assignment_list()))
-        for i in range(len(assignments)):
-            temp_string = f"{str(i)}|{str(assignments[i])}\n"
-            f.write(temp_string)
-            f2.write(temp_string)
-        f.close()
-        f2.close()
-        #token_state.txt allows us to check to see if the user has specified a valid key, if so, then we can call other functions
-        #if token_state == 1, then the user has a valid key, else it's 0
-        with open("token_state.txt", "w") as file:
-            file.write('1')
-        return "success!"
-    if open("token_state.txt").read() == "1":
-        if "todo" in user_input.lower()[:5]:
-            #page gives us which page we want to view, can be specified with !todo 2
-            try:
-                page = int(user_input[5:])
-            except:
-                page = 1
-            return todolist.print_todolist(assignments, page)
-        elif "check" in user_input.lower()[:6]:
-            if int(user_input[6:]) > len(assignments):
-                return("Assignment doesnt exist")
-            #this function is in charge of crossing out the assignment in bleh2.txt
-            todo.checktodo(user_input[6:])
-            return f"Successfully checked out assignment {user_input[6:]}"
-        else:
-            return("Please use a valid command.")
+    if "help" in lowered:
+        #returns a list of commands
+        return(f"**!guide** -> shows you how to get your Canvas API Token\n**!settoken (token)** -> sets your Canvas API token so we can access your classes and assignments\n**!todo** -> returns a list of assignments; you can swap pages using !todo (page number)\n**!check (assignment num)** -> crosses off the chosen assignment to mark that you've completed it\n**!remind** -> DM's the user all the assignments that are due within 3 days every 6 hours\n**!cats** -> erm.. cats?!")
+    elif "guide" in lowered:
+        return(f"Head over to https://csulb.instructure.com/profile/settings and scroll down until you see New Access Token")
     else:
-        return ("Please set a valid canvas api token using !settoken (your token)")
+        #writes the user's assignments to 2 files, one is use for crossing out an assignment, other is used for storing data
+        if "settoken" in user_input.lower()[:9]:
+            with open("user_token.txt", "w") as file:
+                file.write(user_input[9:])
+            user = userobject(open("user_token.txt").read())
+            if user.check_token() == False:
+                with open("token_state.txt", "w") as file:
+                    file.write('0')
+                return "Invalid Access Token"
+            f = open("bleh.txt", "w")
+            f2 = open("bleh2.txt", "w")
+            global assignments
+            assignments = copy.copy(list(classassignments.get_assignment_list()))
+            for i in range(len(assignments)):
+                temp_string = f"{str(i)}|{str(assignments[i])}\n"
+                f.write(temp_string)
+                f2.write(temp_string)
+            f.close()
+            f2.close()
+            #token_state.txt allows us to check to see if the user has specified a valid key, if so, then we can call other functions
+            #if token_state == 1, then the user has a valid key, else it's 0
+            with open("token_state.txt", "w") as file:
+                file.write('1')
+            return "success!"
+        if open("token_state.txt").read() == "1":
+            if "todo" in user_input.lower()[:5]:
+                #page gives us which page we want to view, can be specified with !todo 2
+                try:
+                    page = int(user_input[5:])
+                except:
+                    page = 1
+                return todolist.print_todolist(assignments, page)
+            elif "check" in user_input.lower()[:6]:
+                if int(user_input[6:]) > len(assignments):
+                    return("Assignment doesnt exist")
+                #this function is in charge of crossing out the assignment in bleh2.txt
+                todo.checktodo(user_input[6:])
+                return f"Successfully checked out assignment {user_input[6:]}"
+            else:
+                return("Please use a valid command.")
+        else:
+            return ("Please set a valid canvas api token using !settoken (your token)")
